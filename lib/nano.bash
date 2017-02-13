4b17a11afb4d64ebe500ab6ff661699d34aeed4f
Ted Lilley <ted.lilley@accelerated.com>
2017-02-13 17:08:00 -0500

    initial commit

---
+++ lib/nano.bash
@@ -0,0 +1,64 @@
+[[ -n ${_nano:-} ]] && return
+readonly _nano=loaded
+
+errexit () { putserr "$1"; exit "${2:-1}" ;}
+
+includes () {
+  local search_item=$1; shift
+  local item
+
+  for item in "$@"; do
+    [[ $item == "$search_item" ]] && return
+  done
+  return 1
+}
+
+joina   () { local IFS=$1; shift; puts "$*" ;}
+puts    () { printf '%s\n' "$1"             ;}
+putserr () { puts "$1" >&2                  ;}
+
+ret () {
+  [[ $(_type "$2") == [aA] ]] && { _set_ary "$@"; return ;}
+  _set_scalar "$@"
+}
+
+_set_ary () {
+  if [[ $1 == "$2" ]]; then
+    local _ref
+    _ref=$(declare -p "$2")
+    eval "${_ref/$2/_ref}"
+  else
+    local -n _ref=$2
+  fi
+  local _key
+
+  unset -v "$1" || return
+  eval "$1=()"
+  for _key in "${!_ref[@]}"; do
+    printf -v "$1[$_key]" '%s' "${_ref[$_key]}"
+  done
+}
+
+_set_scalar () {
+  unset  -v "$1" || return
+  printf -v "$1" '%s' "$2"
+}
+
+splits () {
+  local delimiter=$1
+  local string=$2
+  local ref=$3
+  local result=()
+
+  IFS=$delimiter read -ra result <<<"$string" ||:
+
+  local "$ref" || return
+  ret "$ref" result
+}
+
+_type () {
+  local declaration
+
+  declaration=$(declare -p "$1" 2>/dev/null) || return
+  echo "${declaration:9:1}"
+}
---
+++ shpec/nano_shpec.bash
@@ -0,0 +1,120 @@
+library=./shpec-helper.bash
+source "$library" 2>/dev/null || source "${BASH_SOURCE%/*}/$library"
+unset -v library
+
+shpec_source lib/nano.bash
+
+describe 'joina'
+  it "joins an array with a delimiter"
+    declare -a samples=([0]=zero [1]=one)
+    assert equal 'zero@one' "$(joina '@' "${samples[@]}")"
+  end
+
+  it "joins an array with one item"
+    declare -a samples=([0]=zero)
+    assert equal 'zero' "$(joina ';' "${samples[@]}")"
+  end
+end
+
+describe 'puts'
+  it "outputs a string on stdout"
+    assert equal sample "$(puts 'sample')"
+  end
+end
+
+describe 'putserr'
+  it "outputs a string on stderr"
+    assert equal sample "$(putserr 'sample' 2>&1)"
+  end
+end
+
+describe 'ret'
+  it "returns an array in a named variable"
+    results=()
+    samplef () { local samples=( one ); local "$1" || return; ret "$1" samples ;}
+    samplef results
+    printf -v expected 'declare -a results=%s([0]="one")%s' \' \'
+    assert equal "$expected" "$(declare -p results)"
+  end
+
+  it "returns an array in a named variable when the ref is used locally"
+    results=()
+    samplef () { local samples=( one ); local results=( two ); local "$1" || return; ret "$1" samples ;}
+    samplef results
+    printf -v expected 'declare -a results=%s([0]="one")%s' \' \'
+    assert equal "$expected" "$(declare -p results)"
+  end
+
+  it "returns an array in a named variable when the ref is the same as the return variable"
+    results=()
+    samplef () { local results=( one ); local "$1" || return; ret "$1" results ;}
+    samplef results
+    printf -v expected 'declare -a results=%s([0]="one")%s' \' \'
+    assert equal "$expected" "$(declare -p results)"
+  end
+
+  it "returns a hash in a named variable"
+    declare -A resulth=()
+    samplef () { local -A sampleh=( [one]=1 ); local "$1" || return; ret "$1" sampleh ;}
+    samplef resulth
+    printf -v expected 'declare -A resulth=%s([one]="1" )%s' \' \'
+    assert equal "$expected" "$(declare -p resulth)"
+  end
+
+  it "returns a hash in a named variable when the ref is used locally"
+    declare -A resulth=()
+    samplef () { local -A sampleh=( [one]=1 ); local -A resulth=( [two]=2 ); local "$1" || return; ret "$1" sampleh ;}
+    samplef resulth
+    printf -v expected 'declare -A resulth=%s([one]="1" )%s' \' \'
+    assert equal "$expected" "$(declare -p resulth)"
+  end
+
+  it "returns a hash in a named variable when the ref is the same as the return variable"
+    declare -A resulth=()
+    samplef () { local -A resulth=( [one]=1 ); local "$1" || return; ret "$1" resulth ;}
+    samplef resulth
+    printf -v expected 'declare -A resulth=%s([one]="1" )%s' \' \'
+    assert equal "$expected" "$(declare -p resulth)"
+  end
+
+  it "returns a value in a named variable"
+    result=''
+    samplef () { local "$1" || return; ret "$1" one ;}
+    samplef result
+    assert equal one "$result"
+  end
+
+  it "returns a value in a named variable when the ref is used locally"
+    result=''
+    samplef () { local result=one; local "$1" || return; ret "$1" "$result" ;}
+    samplef result
+    assert equal one "$result"
+  end
+end
+
+describe 'splits'
+  it "splits a string into an array on a partition character"
+    results=()
+    sample='a=b'
+    splits '=' "$sample" results
+    printf -v expected 'declare -a results=%s([0]="a" [1]="b")%s' \' \'
+    assert equal "$expected" "$(declare -p results)"
+  end
+end
+
+describe '_type'
+  it "identifies a string by name"
+    sample=''
+    assert equal - "$(_type sample)"
+  end
+
+  it "identifies an array by name"
+    samples=()
+    assert equal a "$(_type samples)"
+  end
+
+  it "identifies a hash by name"
+    declare -A sampleh=()
+    assert equal A "$(_type sampleh)"
+  end
+end
