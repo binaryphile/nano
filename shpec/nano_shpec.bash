436bcb2b7b95e1cea2d36412ca6b3c6afb9f5d7a
Ted Lilley <ted.lilley@accelerated.com>
2017-07-04 23:08:37 -0400

    wip

--- shpec/nano_shpec.bash
+++ shpec/nano_shpec.bash
@@ -26,7 +26,7 @@ describe 'die'
   it "exits with a default error code of 1"; (
     # stop_on_error off
     (die 2>&1)
-    assert equal 1 "$?"
+    assert equal 1 $?
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 
@@ -40,7 +40,7 @@ describe 'die'
   it "exits with an error code"; (
     # stop_on_error off
     (die '' 2 2>&1)
-    assert equal 2 "$?"
+    assert equal 2 $?
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 end
@@ -53,7 +53,55 @@ describe 'decouple'
 
   it "doesn't split a string by name with a delimiter"
     sample=one@two
-    decouple sample on '@'
+    decouple sample on @
     assert equal '([0]="sample")' "$__"
+  end
+end
+
+describe 'grab'
+  it "instantiates a key/value pair from a hash literal as a local"
+    result=$(grab one from '([one]=1)')
+    assert equal 'eval local one=1' "$result"
+  end
+
+  it "instantiates more than one key/value pair from a hash literal"
+    result=$(grab '(one two)' from '([one]=1 [two]=2)')
+    assert equal 'eval local one=1;local two=2' "$result"
+  end
+
+  it "instantiates all key/value pairs from a hash literal"
+    result=$(grab '*' from '([one]=1 [two]=2 [three]=3)')
+    assert equal 'eval local one=1;local two=2;local three=3' "$result"
+  end
+
+  it "instantiates a key/value pair from a hash literal reference"
+    sample='([one]=1)'
+    result=$(grab one from sample)
+    assert equal 'eval local one=1' "$result"
+  end
+
+  it "instantiates more than one key/value pair from a hash literal reference"
+    sample='([one]=1 [two]=2)'
+    result=$(grab '(one two)' from sample)
+    assert equal 'eval local one=1;local two=2' "$result"
+  end
+
+  it "instantiates all key/value pairs from a hash literal reference"
+    sample='([one]=1 [two]=2 [three]=3)'
+    result=$(grab '*' from sample)
+    assert equal 'eval local one=1;local two=2;local three=3' "$result"
+  end
+
+  it "errors if \$3 isn't 'from'"; (
+    # stop_on_error off
+    grab one two
+    assert unequal 0 $?
+    return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
+  end
+
+  it "doesn't work if the first argument is a reference"
+    sample=one
+    result=$(grab sample from '([one]=1)')
+    assert equal "eval local sample=''" "$result"
   end
 end
